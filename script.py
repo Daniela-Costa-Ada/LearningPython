fbe99e0ca37b40b715b845dd3af8c2f5e5818a7f
Daniela <daniela.costa.1031459@sga.pucminas.br>
2020-07-06 16:38:53 -0300

    comandos python atribuiçao e variaveis

--- script.py
+++ script.py
@@ -1,4 +1,14 @@
-if 1 < 2 :
-    print ("OK")
+#comentario em python
+if 1 > 2 :
+    print ("OK")#na frente do codigo
 else:
     print("não ok")
+    valor1 = 10
+    valor_2 = 2.5
+    valor3 = 1j
+    print(valor1)
+    valor_1 = 10
+    # string 
+    nome = "João"
+    apelido = "Ribeiro"
+    print (nome + " " + apelido)
